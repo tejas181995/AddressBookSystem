96a466193776736bccdccbd148ee4ad9aeb38467
tejasdev18 <65755601+tejas181995@users.noreply.github.com>
2021-02-24 20:42:23 +0530

    Created contact in address list

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,55 +1,28 @@
 import java.util.Scanner;
+
 class Contact{
-    String firstName, lastName, address, city, state, zipCode, phoneNumber, email;
-    public Contact(String firstName, String lastName, String address, String city, String state, String zipCode,
-            String phoneNumber, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
-    public void printContact(){
-        System.out.println("First Name : " + firstName);
-        System.out.println("Last Name : " + lastName);
-        System.out.println("Address: " + address);
-        System.out.println("city: " + city);
-        System.out.println("State: " + state);
-        System.out.println("Zip code: " + zipCode);
-        System.out.println("Phone Number: " + phoneNumber);
-        System.out.println("E-mail: " + email);
-
-    }
-}
-class AddressBookSystem {
-  
-    public static void main(String[] args) {
-        String firstName, lastName, address, city, state, zipCode, phoneNumber, email;
-        System.out.println("Welcome to Address Book System");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first Name");
-        firstName = sc.nextLine();
-        System.out.println("Enter Last Name");
-        lastName = sc.nextLine();
-        System.out.println("Enter Address");
-        address = sc.nextLine();
-        System.out.println("Enter City Name");
-        city = sc.nextLine();
-        System.out.println("Enter state Name");
-        state = sc.nextLine();
-        System.out.println("Enter zip code");
-        zipCode = sc.nextLine();
-        System.out.println("Enter Phone Number");
-        phoneNumber = sc.nextLine();
-        System.out.println("Enter E-mail");
-        email = sc.nextLine();
-
-        Contact contact = new Contact(firstName, lastName, address, city, state, zipCode, phoneNumber, email);
-        contact.printContact();
+    public static final String[] fields = new String[]{"firstName", "lastName", "address", "city", "state", "zipCode", "phoneNumber", "email" } ;
+    private String[] values;
+    public Contact(String[] values){
+        this.values = values;
         
     }
+    public void printContact(){
+        for(int i=0; i<fields.length; i++){
+            System.out.println(fields[i] + ":" + values[i]);
+        }
+    }
 }
 
+class AddressBookSystem{
+    public static void main(String[] args) {
+        String[] values = new String[8];
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<values.length; i++){
+            System.out.println("enter" + Contact.fields[i] + ": ");
+            values[i] = sc.nextLine();
+        }
+        Contact contact = new Contact(values);
+        contact.printContact();
+    }
+}
